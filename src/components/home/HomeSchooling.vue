8d5dd95a5490bb4f7dbbfaf5e89d82832c39d168
matteogaillard <matteo.gaillard@protonmail.com>
2024-05-13 16:25:33 +0200

    added schooling feature

---
+++ src/components/home/HomeSchooling.vue
@@ -0,0 +1,42 @@
+<template>
+  <article class="part" id="schooling">
+    <h1 class="title">{{ $t("home.schooling.title") }}</h1>
+
+    <section>
+      <article :class="{'actual': index === 0}" v-for="(education, index) in schooling" :key="education.title">
+        <span class="year year-to">{{ education.years.to ?? "AUJ" }}</span>
+        <span class="year year-from">{{ education.years.from }}</span>
+
+        <img :src="education.thumbnailURL" :alt="`${education.establishment}'s logo`" />
+        <div class="description" role="contentinfo">
+          <h3>{{ education.title }}</h3>
+          <h4>{{ education.establishment }} - {{ education.localization.cityName }} ({{ education.localization.regionCode }}), {{ education.localization.countryName }}</h4>
+          <ul>
+            <li v-for="line in education.description" :key="line">
+              {{line}}
+            </li>
+          </ul>
+
+          <nav>
+            <ul>
+              <li v-for="link in education.links" :key="link.label">
+                <a v-if="link.type === 'REDIRECTION'" :href="link.href" rel="noreferrer" target="_blank" class="icon icon-redirect">
+                  {{ link.label }}
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </article>
+    </section>
+  </article>
+</template>
+
+<script setup>
+import schooling from "@/data/schooling.json";
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/components/home/home-base.scss";
+@import "@/scss/components/home/home-schooling.scss";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <home-hero-header />
   <home-about />
-  <home-education />
+  <home-schooling />
   <home-experiences />
   <home-contact />
 </template>
@@ -9,7 +9,7 @@
 <script setup>
 import HomeHeroHeader from "@/components/home/HomeHeroHeader.vue";
 import HomeAbout from "@/components/home/HomeAbout.vue";
-import HomeEducation from "@/components/home/HomeEducation.vue";
+import HomeSchooling from "@/components/home/HomeSchooling.vue";
 import HomeExperiences from "@/components/home/HomeExperiences.vue";
 import HomeContact from "@/components/home/HomeContact.vue";
 </script>
