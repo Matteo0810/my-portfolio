f36b994d4e26843348bb12200d01ee3dde248b69
matteogaillard <matteo.gaillard@protonmail.com>
2024-05-19 00:46:51 +0200

    fix env

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL || "./",
+  base: process.env.VITE_BASE_URL || "./",
   test: {
     environment: "happy-dom"
   },
