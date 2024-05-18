f1c9a1f954fb81768da91b9ab722d012f4a513b5
matteogaillard <matteo.gaillard@protonmail.com>
2024-05-19 00:49:13 +0200

    fixed env

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || "./",
+  base: "/my-portfolio/",
   test: {
     environment: "happy-dom"
   },
